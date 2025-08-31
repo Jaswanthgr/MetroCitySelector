f1f43c8708ce6a6d5cd10c696ae538b3d8e2ba69
Jaswanth Reddy G <jashwanthgr@gmail.com>
2025-08-31 19:54:01 +0530

    Added Exception

--- Cities.aspx.cs
+++ Cities.aspx.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Web.Services;
-using System.Data.SqlClient;
 using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web.Services;
 
 namespace MetroCityDemo
 {
@@ -21,23 +22,40 @@ namespace MetroCityDemo
         public static List<CityInfo> GetCities()
         {
             var cities = new List<CityInfo>();
-            string connStr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            try
+            {                
+                string connStr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
 
-            using (SqlConnection conn = new SqlConnection(connStr))
-            {
-                conn.Open();
-                using (SqlCommand cmd = new SqlCommand("SELECT city, ismetro FROM CityMetro", conn))
-                using (SqlDataReader reader = cmd.ExecuteReader())
+                using (SqlConnection conn = new SqlConnection(connStr))
                 {
-                    while (reader.Read())
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("SELECT city, ismetro FROM CityMetro", conn))
+                    using (SqlDataReader reader = cmd.ExecuteReader())
                     {
-                        cities.Add(new CityInfo
+                        while (reader.Read())
                         {
-                            City = reader.GetString(0),
-                            IsMetro = reader.GetBoolean(1)
-                        });
+                            cities.Add(new CityInfo
+                            {
+                                City = reader.GetString(0),
+                                IsMetro = reader.GetBoolean(1)
+                            });
+                        }
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                string logFilePath = ConfigurationManager.AppSettings["LogFilePath"] ?? "error_log.txt";
+                string logDirectory = Path.GetDirectoryName(logFilePath);
+
+                // Create directory if it doesn't exist
+                if (!Directory.Exists(logDirectory) && !string.IsNullOrEmpty(logDirectory))
+                {
+                    Directory.CreateDirectory(logDirectory);
+                }
+
+                string logMessage = $"[{DateTime.Now}] Exception: {ex.Message}\nStack Trace: {ex.StackTrace}\n";
+                File.AppendAllText(logFilePath, logMessage);
             }
             return cities;
         }
